892007752973552753da30158fa5f212d24ce447
junjiezeng <junjie.zeng@focussend.com>
2020-07-15 14:03:21 +0800

    封装ajax

---
+++ src/api/ajax.js
@@ -0,0 +1,21 @@
+/*
+    发送ajax请求的函数模块
+    函数的返回值是promise对象
+
+*/
+// axios
+import axios from 'axios';
+export default function ajax(url ,data={},method = "GET"){
+    //大部分请求都是get所以默认为get
+    if(method === "GET"){
+    	var paramStr = ''
+        Object.keys(data).forEach(key =>{
+             paramStr += `${key}=${data[key]}&`
+        })
+        paramStr = paramStr && paramStr.substring(0,paramStr.length-1)
+        url = url ? `${url}?${paramStr}`:url
+        return axios.get(url)
+    }else{
+    	return axios.post(url,data)
+    }
+}
---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+/*
+    包含了n个接口请求的函数的模块
+    函数返回值为：promise
+*/
+
+// 引入ajax
+import ajax from './ajax';
+
