93eb12a96e8285b11833ff0b3316c468e85d7656
junjiezeng <junjie.zeng@focussend.com>
2020-07-17 17:20:06 +0800

    路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 //renderRoutes 读取路由配置转化为 Route 标签
 import { renderRoutes } from 'react-router-config'
-import routes from './routes/index'
+import {Route,Switch} from 'react-router-dom';
+// import routes from './routes/index'
+
+import  Home  from './container/Home'
+import  Msg  from './container/Msg'
+import  Classify  from './container/Classify'
+import  My  from './container/My'
 function App() {
   return (
     <div >
-      {renderRoutes(routes)}
+      {/* {renderRoutes(routes)} */}
+      <Switch>
+        <Route path = '/home' component = {Home}></Route>
+        <Route path = '/msg' component = {Msg}></Route>
+        <Route path = '/classify' component = {Classify}></Route>
+        <Route path = '/my' component = {My}></Route>
+      </Switch>
     </div>
   );
 }
