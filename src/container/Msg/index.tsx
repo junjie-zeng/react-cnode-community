7c15f39a4447e7dde8448dd730b6c95605250ff3
junjiezeng <junjie.zeng@focussend.com>
2020-07-31 14:35:49 +0800

    静态页面

--- src/container/Msg/index.tsx
+++ src/container/Msg/index.tsx
@@ -1,11 +1,17 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 
 
-class Msg extends Component{
-    render(){
-        return(
-            <div>
-                消息
+class Msg extends Component {
+    render() {
+        return (
+            <div className="msg-box">
+                <div className="msg-info">
+                    <a href="javascript:;" className="active">未读消息</a>
+                    <a href="javascript:;">已读消息</a>
+                </div>
+                <div className="msg-content">
+                    暂无消息
+                </div>
             </div>
         )
     }
--- src/container/My/index.tsx
+++ src/container/My/index.tsx
@@ -1,11 +1,33 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 
 
-class My extends Component{
-    render(){
-        return(
+class My extends Component {
+    render() {
+        return (
             <div>
-                我的
+                <div className="header">
+                    <div className="header-left"><em></em></div>
+                    <div className="header-center">jjz</div>
+                    <div className="header-right"><em></em></div>
+                </div>
+                <div className="my-box">
+                    <div className="wrapper">
+                        <div className="header-protrait">
+
+                        </div>
+                    </div>
+                    <div className="info">
+                        <span>更多信息</span>
+                        <span>&gt;</span>
+                    </div>
+                    <div className="info">
+                        <span>评论加尾</span>
+                        <span>&gt;</span>
+                    </div>
+                    <div className="btn">
+                        <button>登录</button>
+                    </div>
+                </div>
             </div>
         )
     }
