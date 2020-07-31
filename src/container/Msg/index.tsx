b2922e92c0906d074bd6fdff5224549a3bda197c
junjiezeng <junjie.zeng@focussend.com>
2020-07-31 15:05:14 +0800

    静态页面

--- src/container/Msg/index.tsx
+++ src/container/Msg/index.tsx
@@ -12,6 +12,28 @@ class Msg extends Component {
                 <div className="msg-content">
                     暂无消息
                 </div>
+                {/* <div className="list-box">
+                    <div className="list-item">
+                        <div className="item-top">
+                            <div className="top-portrait"></div>
+                            <h3>JAck</h3>
+                            <span className="top-time">8 天前</span>
+                        </div>
+                        <div className="item-content">
+                            <p>hello word</p>
+                        </div>
+                    </div>
+                    <div className="list-item">
+                        <div className="item-top">
+                            <div className="top-portrait"></div>
+                            <h3>JAck</h3>
+                            <span className="top-time">8 天前</span>
+                        </div>
+                        <div className="item-content">
+                            <p>hello word</p>
+                        </div>
+                    </div>
+                </div> */}
             </div>
         )
     }
