f77daf6702e8b517e7d7ebe0c1083fa49e2ec339
junjiezeng <junjie.zeng@focussend.com>
2020-07-31 14:27:13 +0800

    根据分类获取

--- src/api/index.js
+++ src/api/index.js
@@ -10,8 +10,8 @@ const _PATH = 'https://cnodejs.org/api/v1'
 
 // https://cnodejs.org/api/v1/topics?page=1&limit=2&markdown=false
 
-export const getContentListRequest = (page, limit) => ajax(`${_PATH}/topics?page=${page}&limit=${limit}`)
+export const getContentListRequest = (page, limit, tab = '' ) => ajax(`${_PATH}/topics?page=${page}&limit=${limit}${tab ? `&tab=${tab}` : ''}`)
 
 // https://cnodejs.org/api/v1/topic/5ee1ee83b703280f0bcb922a
 
-export const getContentDetailRequest = (id)=>ajax(`${_PATH}/topic/${id}`)
+export const getContentDetailRequest = (id) => ajax(`${_PATH}/topic/${id}`)
