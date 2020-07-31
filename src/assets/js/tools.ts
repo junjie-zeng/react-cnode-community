7a23633fc96a11af35779617f5354647f4a51cec
junjiezeng <junjie.zeng@focussend.com>
2020-07-31 14:27:46 +0800

    工具方法

--- src/assets/js/tools.ts
+++ src/assets/js/tools.ts
@@ -2,13 +2,24 @@ import 'moment/locale/zh-cn'
 
 
 
-export const getThemeType = (type:string) => {
-    console.log()
-    return type == 'aks' ? '问答' : type == 'share' ? '分享' : type == 'job' ? '职位' : '满意'
+export const getThemeType = (tab: string) => {
+    // console.log(tab)
+    switch (tab) {
+        case 'ask':
+            return '问答'
+        case 'share':
+            return '分享'
+        case 'job':
+            return '招聘'
+        case 'good':
+            return '精华'
+        default:
+            return ''
+    }
 }
 
 
-export const getRelativeTime = (date:string) => {
+export const getRelativeTime = (date: string) => {
     var moment = require('moment');
-    return moment(date, "YYYYMMDD").fromNow().replace(/[\s]+/g,'')
+    return moment(date, "YYYYMMDD").fromNow().replace(/[\s]+/g, '')
 }
