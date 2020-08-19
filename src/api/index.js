6d2b0486ee01c18d5ab07af7ee2e227f4350c433
junjiezeng <junjie.zeng@focussend.com>
2020-08-19 16:40:26 +0800

    增加接口

--- src/api/index.js
+++ src/api/index.js
@@ -21,7 +21,13 @@ export const getUserDetailRequest = (username) => ajax(`${_PATH}/user/${username
 
 // 获取消息
 // https://cnodejs.org/api/v1/messages?accesstoken=xxxx
-export const getMsgRequest = (accesstoken)=>ajax(`${_PATH}/messages?accesstoken=${accesstoken}`)
+export const getMsgRequest = (accesstoken) => ajax(`${_PATH}/messages?accesstoken=${accesstoken}`)
+// 获取未读消息数量
+export const getUnreadMsgCountRequest = (accesstoken) => ajax(`${_PATH}/message/count?accesstoken=${accesstoken}`)
 // 效验token
 export const verifyTokenRequest = (accesstoken) => ajax(`${_PATH}/accesstoken`, { accesstoken }, 'POST')
 
+// 主题收藏
+export const collectRequest = (accesstoken, topic_id) => ajax(`${_PATH}/topic_collect/collect`, { accesstoken, topic_id }, 'POST')
+// 取消主题收藏
+export const delCollectRequest = (accesstoken, topic_id) => ajax(`${_PATH}/topic_collect/de_collect`, { accesstoken, topic_id }, 'POST')
