71875cbe857e991b5b96c02a5c455027ea00514a
junjiezeng <junjie.zeng@focussend.com>
2020-08-24 14:55:35 +0800

    数据变化滚动条吸顶

--- src/baseUi/Scroll/index.tsx
+++ src/baseUi/Scroll/index.tsx
@@ -19,19 +19,23 @@ class Scroll extends React.Component<Props, State>{
         this.state = {
             dropDownRefresh: false
         }
+
     }
+
+    wrapper: any
+
     // 第一次渲染后
     componentDidMount() {
-        const _wrapper:any = this.refs.wrapper
+        const _wrapper: any = this.refs.wrapper
         // console.log(this)
-        const wrapper = new BScroll(_wrapper, {
+        this.wrapper = new BScroll(_wrapper, {
             scrollY: true,
             click: true,
             probeType: 2,
         })
 
         // 手指离开触发（模拟下拉刷新）
-        wrapper.on('touchEnd', (ev: any) => {
+        this.wrapper.on('touchEnd', (ev: any) => {
             // 判断下拉动作
             if (ev.y > 30) {
                 // console.log(ev.y)
@@ -40,25 +44,40 @@ class Scroll extends React.Component<Props, State>{
             this.setState({
                 dropDownRefresh: false
             })
+            // console.log(ev)
         })
 
-        wrapper.on('scroll', (ev) => {
-            // console.log("test")
+        this.wrapper.on('scroll', (ev: any) => {
+            // console.log(ev)
             this.setState({
                 dropDownRefresh: true
             })
         })
-
     }
+
+    // 在组件接收到新的props或者state但还没有render时被调用
+    componentWillReceiveProps() {
+        this._setScrollTo()
+    }
+
+    // 数据更新滚动条重置
+    _setScrollTo() {
+        this.wrapper.scrollTo(0, 0)
+        this.setState({
+            dropDownRefresh: false
+        })
+    }
+
     render() {
         const { children, refreshTips } = this.props
         const { dropDownRefresh } = this.state
+        const _style = { paddingBottom: '2rem' }
         return (
-            <div className="scroll-box" ref = "wrapper">
-                <div className="scroll-wrap-list" style={{ paddingBottom: '2rem' }}>
+            <div className="scroll-box" ref="wrapper">
+                <div className="scroll-wrap-list" style={_style}>
                     <Tips lei="refresh-tips" isShow={dropDownRefresh} >释放即可刷新</Tips>
                     {/* <Tips lei="refresh-tips" isShow={refreshTips} >加载中...</Tips> */}
-                    <RefreshLoading isShow={refreshTips}/>
+                    <RefreshLoading isShow={refreshTips} />
                     {children}
                 </div>
             </div>
